6da64579b9796c8f462604783fb5d5c7917f10e9
otavioHirooka <otaviolbh@hotmail.com>
2020-07-21 11:09:30 -0300

    Animações

--- animacoes/animacoes-exercicios/src/App.vue
+++ animacoes/animacoes-exercicios/src/App.vue
@@ -25,18 +25,104 @@
 			<b-alert variant="info" show v-if="exibir" key="info">{{ msg }}</b-alert>
 			<b-alert variant="warning" show v-else key="warn">{{ msg }}</b-alert>
 		</transition>
+		<hr>
+		<button @click="exibir2 = !exibir2">Alternar</button>
+		<transition
+			:css="false"
+			@before-enter="beforeEnter"
+			@enter="enter"
+			@after-enter="afterEnter"
+			@enter-cancelled="enterCancelled"
+			
+			@before-leave="beforeLeave"
+			@leave="leave"
+			@after-leave="afterLeave"
+			@leave-cancelled="leaveCancelled">
+			<div v-if="exibir2" class="caixa"></div>
+		</transition>
+		<hr>
+		<div class="mb-4">
+			<b-button variant="primary" class="mr-2"
+				@click="componenteSelecionado = 'AlertaInfo'">Info</b-button>
+			<b-button variant="secundary" 
+				@click="componenteSelecionado = 'AlertaAdvertencia'">Advertência</b-button>
+		</div>
+		<transition name="fade" mode="out-in">
+			<componente :is="componenteSelecionado"></componente>
+		</transition>
+		<hr>
+		<b-button @click="adicionarCav" class="mb-4">Adicionar Cavaleiro</b-button>
+		<transition-group name="slide" tag="div">
+			<b-list-group v-for="(cav, i) in cavaleiros" :key="cav">
+				<b-list-group-item @click="removerCav(i)">{{ cav }}</b-list-group-item>
+			</b-list-group>
+		</transition-group>
 	</div>
 </template>
 
 <script>
+import AlertaAdvertencia from './AlertaAdvertencia.vue'
+import AlertaInfo from './AlertaInfo.vue'
 
 export default {
+	components: { AlertaAdvertencia, AlertaInfo },
 	data() {
 		return{
+			cavaleiros: ['Kanon', 'Saga', 'Shaka', 'Milo'],
 			msg: 'Uma mensagem de informação para o usuário',
 			exibir: false,
-			tipoAnimacao: 'fade'
+			exibir2: true,
+			tipoAnimacao: 'fade',
+			larguraBase: 0,
+			componenteSelecionado: 'AlertaInfo'
 		}
+	},
+	methods: {
+		adicionarCav() {
+			const s = Math.random().toString(36).substring(2)
+			this.cavaleiros.push(s)
+		},
+		removerCav(indice) {
+			this.cavaleiros.splice(indice, 1)
+		},
+		animar(el, done, negativo) {
+			let rodada = 1
+			const temporizador = setInterval(() => {
+				const novaLargura = this.larguraBase + (negativo ? -rodada * 10 : rodada * 10)
+				el.style.width = `${novaLargura}px`
+				rodada++
+				if (rodada > 30) {
+					clearInterval(temporizador)
+					done()
+				}
+			}, 20);
+		},
+		beforeEnter(el) {
+			this.larguraBase = 0
+			el.style.width = `${this.larguraBase}px`
+		},
+		enter(el, done) {
+			this.animar(el, done, false)
+		},
+		// afterEnter(el) {
+		// 	console.log('afterEnter')
+		// },
+		// enterCancelled(el) {
+		// 	console.log('afterCancelled')
+		// },
+		beforeLeave(el) {
+			this.larguraBase = 300
+			el.style.width = `${this.larguraBase}px`
+		},
+		leave(el, done) {
+			this.animar(el, done, true)
+		},
+		// afterLeave(el) {
+		// 	console.log('afterLeave')
+		// },
+		// leaveCancelled(el) {
+		// 	console.log('leaveCancelled')
+		// },
 	}
 }
 </script>
@@ -50,6 +136,13 @@ export default {
 	color: #2c3e50;
 	margin-top: 60px;
 	font-size: 1.5rem;
+}
+
+.caixa {
+	height: 100px;
+	width: 300px;
+	margin: 30px auto;
+	background-color: lightgreen;
 }
 
 /* -- forma reduzida */
@@ -104,6 +197,8 @@ export default {
 }
 
 .slide-leave-active {
+	position: absolute;
+	width: 100%;
 	animation: slide-out 2s ease;
 	transition: opacity 2s;
 
@@ -111,6 +206,9 @@ export default {
 
 .slide-enter, .slide-leave-to {
 	opacity: 0;
+}
+.slide-move {
+	transition: transform 1s;
 }
 
 </style>
